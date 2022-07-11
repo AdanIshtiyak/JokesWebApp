2ee4d2b6ac995bd76e02e37afce16354f3bc2c96
Adan <adan.ishtiyak@gmail.com>
2022-07-11 12:19:56 +0600

    NewSaveVers

---
+++ NewSaveVers/NewSaveVers/Models/DB.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NewSaveVers.Models
+{
+    public class DB : DbContext
+    {
+        public DbSet<Analyzes> Analyzes { get; set; }
+        public DbSet<Norms> Norms { get; set; }
+    }
+}
---
+++ NewSaveVers/NewSaveVers/Models/Norms.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewSaveVers.Models
+{
+    public class Analyzes
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Norms> Norms { get; set; }
+    }
+    public class Norms
+    {
+        public int ID { get; set; }
+        public int AnalyzID { get; set; }
+        public string Name { get; set; }
+        public double MinValue1 { get; set; }
+        public double MaxValue1 { get; set; }
+        public double MinValue2 { get; set; }
+        public double MaxValue2 { get; set; }
+
+        [ForeignKey("AnalyzesID")]
+        public Analyzes Analyzes { get; set; }
+    }
+}
